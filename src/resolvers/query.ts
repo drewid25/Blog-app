a7e9c9db5cf123793777ccb07393a3ab3d0f3112
unknown <andrewidanol@gmail.com>
2022-03-20 18:55:11 +0800

    updated query post

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,3 +1,16 @@
+
+import { Context } from "../index";
+
 export const Query ={
-    hello: ()=>"World"
+   posts: (parent:any, args:any,{prisma}:Context)=>{
+   return prisma.post.findMany({
+         orderBy:[
+             {
+                 createdAt:"desc"
+             },
+             
+         ],
+    })
+   
+}
 }
